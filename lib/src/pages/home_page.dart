b274535155659367fa39177a177b753680525c83
CristianX <thecristianx@hotmail.com>
2020-05-12 17:43:30 -0500

    Implementando menú lateral con drawer en el home_page

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,4 +1,8 @@
+
 import 'package:flutter/material.dart';
+
+// Vistas
+import 'package:preferenciasusuario/src/pages/settings_page.dart';
 
 class HomePage extends StatelessWidget {
 
@@ -12,6 +16,10 @@ static final String routeName = 'home';
       appBar: AppBar( 
         title: Text('Preferencias de usuario'),
        ),
+
+      //  Menu lateral
+      drawer: _crearMenu( context ),
+
        body: Column(
          mainAxisAlignment: MainAxisAlignment.center,
          children: <Widget>[
@@ -24,6 +32,56 @@ static final String routeName = 'home';
          ],
        ),
       
+    );
+  }
+
+  Drawer _crearMenu( BuildContext context ) {
+    return Drawer(
+
+      // Lo tradicional es almacenar un ListView
+      child: ListView(
+        // Para que ocupe todo el espacio
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            child: Container(),
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/original.jpg'),
+                fit: BoxFit.cover
+              )
+            )
+          ),
+          // Opciones del menú
+          ListTile(
+            leading: Icon( Icons.pages, color: Colors.blue ),
+            title: Text('Home'),
+            onTap: () => Navigator.pushReplacementNamed(context, HomePage.routeName),
+          ),
+          ListTile(
+            leading: Icon( Icons.party_mode, color: Colors.blue ),
+            title: Text('Party Mode'),
+            onTap: (){},
+          ),
+          ListTile(
+            leading: Icon( Icons.people, color: Colors.blue ),
+            title: Text('People'),
+            onTap: (){},
+          ),
+          ListTile(
+            leading: Icon( Icons.settings, color: Colors.blue ),
+            title: Text('Configuración'),
+            onTap: (){
+              // Para cerrar menú lateral
+              // Navigator.pop(context);
+
+              // Para cambiar vista raíz
+              Navigator.pushReplacementNamed(context, SettingsPage.routeName);
+            },
+          ),
+        ],
+      ),
+
     );
   }
 }
