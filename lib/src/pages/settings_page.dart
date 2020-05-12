c56783c1781d7e8d447ce1ad2abb6fc4cdf40624
CristianX <thecristianx@hotmail.com>
2020-05-12 18:16:53 -0500

    Implementando diseño de la pantalla settings_page

--- lib/src/pages/settings_page.dart
+++ lib/src/pages/settings_page.dart
@@ -3,10 +3,31 @@ import 'package:flutter/material.dart';
 // Widget
 import 'package:preferenciasusuario/src/widgets/menuLateral_widget.dart';
 
-class SettingsPage extends StatelessWidget {
+class SettingsPage extends StatefulWidget {
 
 // Definiendo nombre de la página para otra forma de enrutamiento
 static final String routeName = 'settings';
+
+  @override
+  _SettingsPageState createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+
+  bool _colorSecundario = false;
+  int _genero = 1;
+  String _nombre = 'Cristian';
+
+  // Controller del campo de teto
+  TextEditingController _textController;
+
+  // Cuando se inicie el página
+  @override
+  void initState() { 
+    super.initState();
+    _textController = new TextEditingController( text: _nombre );
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -17,8 +38,61 @@ static final String routeName = 'settings';
        ),
       //  Aplicando menú lateral
        drawer: MenuLateralWidget(),
-       body: Center(
-         child: Text('Ajustes Page'),
+       body: ListView(
+         children: <Widget>[
+           Container(
+             padding: EdgeInsets.all(5.0),
+             child: Text('Configuración', style: TextStyle( fontSize: 45.0, fontWeight: FontWeight.bold )),
+           ),
+
+          //  Crea una línea de separación
+           Divider(),
+           SwitchListTile(
+             value: _colorSecundario, 
+             title: Text('Color secundario'),
+             onChanged: ( value ){ 
+              //  Para redibujar
+               setState(() {
+               _colorSecundario = value;
+               });
+              },
+            ),
+
+            RadioListTile(
+              value: 1, 
+              title: Text('Masculino'),
+              groupValue: _genero,
+              onChanged: ( value ) { 
+                setState(() {
+                _genero = value;
+                });
+              },
+            ),
+            RadioListTile(
+              value: 2, 
+              title: Text('Femenino'),
+              groupValue: _genero,
+              onChanged: ( value ) { 
+                setState(() {
+                _genero = value;
+                });
+              },
+            ),
+            
+            Divider(),
+
+            Container(
+              padding: EdgeInsets.symmetric( horizontal: 20.0 ),
+              child: TextField(
+                controller: _textController,
+                decoration: InputDecoration(
+                  labelText: 'Nombre',
+                  helperText: 'Nombre completo'
+                ),
+                onChanged: ( value ) {},
+              ),
+            )
+         ],
        )
       
     );
